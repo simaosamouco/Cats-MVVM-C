44237d9ddf0f69a2b25b0e0c8eb0509373cdd333
Simão Neves Samouco <simao.samouco@hotmail.com>
2025-09-13 10:17:33 +0100

    Initial Commit

---
+++ CatsTests/CatsTests.swift
@@ -0,0 +1,17 @@
+//
+//  CatsTests.swift
+//  CatsTests
+//
+//  Created by Simão Neves Samouco on 13/09/2025.
+//
+
+import Testing
+@testable import Cats
+
+struct CatsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
