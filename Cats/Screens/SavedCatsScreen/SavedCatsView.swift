094f6aedebd473d2733eedc00bf958c133d99036
Simão Samouco <simao.samouco@hotmail.com>
2026-03-07 13:51:46 +0000

    Saved cats view organizsed

--- Cats/Screens/SavedCatsScreen/SavedCatsView.swift
+++ Cats/Screens/SavedCatsScreen/SavedCatsView.swift
@@ -13,13 +13,13 @@ struct SavedCatsView<ViewModel: SavedCatsViewModelProtocol>: View  {
     
     @State private var isGridView = true
     
-    let columns = [
+    private let columns = [
         GridItem(.flexible()),
         GridItem(.flexible()),
         GridItem(.flexible())
     ]
     
-    let listColumns = [
+    private let listColumns = [
         GridItem(.flexible())
     ]
     
@@ -28,58 +28,75 @@ struct SavedCatsView<ViewModel: SavedCatsViewModelProtocol>: View  {
             ScrollView {
                 VStack {
                     if viewModel.isLoading {
-                        ProgressView()
-                            .progressViewStyle(CircularProgressViewStyle())
-                            .scaleEffect(1.5)
+                        loader
                     }
                     
                     if viewModel.cats.isEmpty {
-                        VStack(spacing: Measures.Spacing.regular) {
-                            Spacer()
-                            Image(systemName: "bookmark.slash")
-                                .resizable()
-                                .frame(width: Measures.Size.huge, height: Measures.Size.huge)
-                            Text("savedCats.emptyState.title".localized)
-                                .font(.headline)
-                            Text("savedCats.emptyState.subtitle".localized)
-                                .font(.subheadline)
-                                .foregroundStyle(.secondary)
-                                .multilineTextAlignment(.center)
-                                .padding(.horizontal, Measures.Spacing.xWide)
-                        }
-                        .scrollDisabled(true)
+                        emptyState
                     } else {
-                        LazyVGrid(columns: isGridView ? columns : listColumns,
-                                  spacing: Measures.Spacing.small) {
-                            ForEach(viewModel.cats, id: \.id) { catCellViewModel in
-                                CatCell(viewModel: catCellViewModel)
-                                    .onTapGesture {
-                                        viewModel.didTapCat(catCellViewModel)
-                                    }
-                            }
-                        }
-                        .padding(.horizontal, Measures.Spacing.regular)
-                        .padding(.top, Measures.Spacing.regular)
+                        catsList
                     }
                 }
             }
             
-            // Floating button
             if !viewModel.cats.isEmpty {
-                FloatingButton(
-                    image: isGridView ? Image(systemName: "list.bullet") : Image(systemName: "square.grid.3x3"),
-                    action: {
-                        withAnimation(.easeInOut(duration: 0.3)) {
-                            isGridView.toggle()
-                        }
-                    }
-                )
-                .contentTransition(.symbolEffect(.automatic))
+                toggleLayoutButton
             }
         }
         .onAppear {
             viewModel.getSavedCats()
         }
     }
+    
+    // MARK: Subviews
+    
+    private var loader: some View {
+        ProgressView()
+            .progressViewStyle(CircularProgressViewStyle())
+            .scaleEffect(1.5)
+    }
+    
+    private var emptyState: some View {
+        VStack(spacing: Measures.Spacing.regular) {
+            Spacer()
+            Image(systemName: "bookmark.slash")
+                .resizable()
+                .frame(width: Measures.Size.huge, height: Measures.Size.huge)
+            Text("savedCats.emptyState.title".localized)
+                .font(.headline)
+            Text("savedCats.emptyState.subtitle".localized)
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, Measures.Spacing.xWide)
+        }
+        .scrollDisabled(true)
+    }
+    
+    private var catsList: some View {
+        LazyVGrid(
+            columns: isGridView ? columns : listColumns,
+            spacing: Measures.Spacing.small
+        ) {
+            ForEach(viewModel.cats, id: \.id) { catCellViewModel in
+                CatCell(viewModel: catCellViewModel)
+                    .onTapGesture { viewModel.didTapCat(catCellViewModel) }
+            }
+        }
+        .padding(.horizontal, Measures.Spacing.regular)
+        .padding(.top, Measures.Spacing.regular)
+    }
+    
+    private var toggleLayoutButton: some View {
+        FloatingButton(
+            image: isGridView ? Image(systemName: "list.bullet") : Image(systemName: "square.grid.3x3"),
+            action: {
+                withAnimation(.easeInOut(duration: 0.3)) {
+                    isGridView.toggle()
+                }
+            }
+        )
+        .contentTransition(.symbolEffect(.automatic))
+    }
+    
 }
-
